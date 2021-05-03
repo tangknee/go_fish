fd3c1f5496323df6b55c7149056787d5e6527089
tangknee <jtangney1@gmail.com>
2021-05-03 19:44:05 -0400

    Rearrange functions and clean up code

--- go_fish.py
+++ go_fish.py
@@ -14,6 +14,49 @@ deck = Cards.shuffle_cards(game_deck)
 user_hand = Cards.draw_cards(deck, 15)
 cpu_hand = Cards.draw_cards(deck, 15)
 
+
+def book_management(player):
+    
+
+
+    point = 0
+    point = check_book(player)
+
+
+
+
+    user_pt_adder = 0
+    cpu_pt_adder = 0
+
+    if player == user_hand:
+        if point > 0:
+            user_pt_adder = point + user_pt_adder
+    if player == cpu_hand:
+        if point > 0:
+            cpu_pt_adder = point + cpu_pt_adder
+
+    global user_points
+    global cpu_points
+
+    user_points = user_points + user_pt_adder
+    cpu_points = cpu_points + cpu_pt_adder
+
+    if user_pt_adder > 0 or cpu_pt_adder > 0:
+        print('You have ' + str(user_points) + ' books.')
+        print('CPU has ' + str(cpu_points) + ' books.')
+        print()
+
+    if user_points > 6:
+        print()
+        print('YOU WIN')
+        sys.exit()
+    if cpu_points > 6:
+        print()
+        print('YOU LOSE')
+        sys.exit()
+
+
+
 def user_request_cards():
     print('You ask: Do you have any... (2-10,JQKA) ? ',end='')
     while True:
@@ -167,79 +210,17 @@ def check_book(hand):
     return point
 
 
-def book_management(player, user_pts, cpu_pts):   #user_bks, cpu_bks): #something is wrong, books are not counting properly
-
-    # user_bks = list(filter(None, user_bks))
-    # cpu_bks = list(filter(None, cpu_bks))
-    #
-    # current_user_bks = len(user_bks)
-    # current_cpu_bks = len(cpu_bks)
-    #
-    #
-    #
-    # user_bks.append((check_book(user_hand)))
-    # user_bks = list(filter(None, user_bks))
-    #
-    # cpu_bks.append((check_book(cpu_hand)))
-    # cpu_bks = list(filter(None, cpu_bks))
-    point = 0
-    point = check_book(player)
-
-
-    # if player == user_hand:
-    #     point
-
-
-    user_pt_adder = 0
-    cpu_pt_adder = 0
-
-    if player == user_hand:
-        if point > 0:
-            user_pt_adder = point + user_pt_adder
-    if player == cpu_hand:
-        if point > 0:
-            cpu_pt_adder = point + cpu_pt_adder
-
-    global user_points
-    global cpu_points
-
-    user_points = user_points + user_pt_adder
-    cpu_points = cpu_points + cpu_pt_adder
-
-    if user_pt_adder > 0 or cpu_pt_adder > 0:
-        print('You have ' + str(user_points) + ' books.')
-        print('CPU has ' + str(cpu_points) + ' books.')
-        print()
-
-    if user_points > 6:
-        print()
-        print('YOU WIN')
-        sys.exit()
-    if cpu_points > 6:
-        print()
-        print('YOU LOSE')
-        sys.exit()
 
 
 
 
-
-    # if len(user_bks) > current_user_bks or len(cpu_bks) > current_cpu_bks:
-    #
-    #     print('You have ' + str(len(user_bks)) + ' books.')
-    #     print('CPU has ' + str(len(cpu_bks)) + ' books.')
-
-#intialize books for each player
-user_books = []
-cpu_books = []
 
 global user_points
 user_points = 0
 global cpu_points
 cpu_points = 0
 
-#checks for books and displays to user
-#book_management(user_hand, user_books, cpu_books)
+
 
 '''game loop'''
 while True:
@@ -254,12 +235,12 @@ while True:
     #user's turn to request if cpu has requested face value
     user_card_requested = user_request_cards()
 
-    book_management(user_hand, user_books, cpu_books)
+    book_management(user_hand)
 
     #check cpu hand with requested card and removes cpu cards
     fishing = check_hand(cpu_hand, user_card_requested)
 
-    book_management(user_hand, user_books, cpu_books)
+    book_management(user_hand)
 
     #check to see if user needs to go fishing
     if fishing is True:
@@ -267,7 +248,7 @@ while True:
         go_fishing(user_hand)
         fishing = False
 
-    book_management(user_hand, user_books, cpu_books)
+    book_management(user_hand)
 
     '''CPU TURN'''
 
@@ -276,11 +257,11 @@ while True:
 
     input()
 
-    book_management(cpu_hand, user_books, cpu_books)
+    book_management(cpu_hand)
 
     fishing = check_hand(user_hand, cpu_card_requested)
 
-    book_management(cpu_hand, user_books, cpu_books)
+    book_management(cpu_hand)
 
     #check to see if CPU needs to go fishing
     if fishing is True:
@@ -289,7 +270,7 @@ while True:
 
         fishing = False
 
-    book_management(cpu_hand, user_books, cpu_books)
+    book_management(cpu_hand)
 
 
     continue
